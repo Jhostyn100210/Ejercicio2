e9ace1126e3a4847878b36bd6ed97d3a513a2664
Jhostyn <Jhostynchacon2@gmail.com>
2024-07-21 10:55:19 -0600

    Main funcionando con las clases  terminado

--- src/main/java/umg/pricipal/Main.java
+++ src/main/java/umg/pricipal/Main.java
@@ -5,6 +5,18 @@ import umg.pricipal.pkg3.clase3;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Área del círculo: " + clase1.areaCirculo(5));
+        System.out.println("Circunferencia del círculo: " + clase1.circunferenciaCirculo(5));
+        System.out.println("Volumen del cubo: " + clase1.volumenCubo(3));
 
+        // PKG Grupo 2
+        System.out.println("Área de la pirámide cuadrada: " + clase2.areaPiramideCuadrada(4, 6));
+        System.out.println("Volumen de la pirámide cuadrada: " + clase2.volumenPiramideCuadrada(4, 6));
+        System.out.println("Área del triángulo: " + clase2.areaTriangulo(4, 6));
+
+        // PKG Grupo 3
+        System.out.println("Volumen del prisma rectangular: " + clase3.volumenPrismaRectangular(24, 10));
+        System.out.println("Área del paralelogramo: " + clase3.areaParalelogramo(4, 6));
+        System.out.println("Volumen del cilindro: " + clase3.volumenCilindro(3, 7));
     }
 }
